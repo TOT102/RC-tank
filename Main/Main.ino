ae467ed7f3f308acb1f758ddf6321b93c50fb0a1
TOT102 <73033834+TOT102@users.noreply.github.com>
2024-04-29 12:26:42 +0200

    Add pin defines
    Add pin defines for DC motor driver

--- Main/Main.ino
+++ Main/Main.ino
@@ -8,8 +8,8 @@ int n;
 
 SBUS sbus(Serial);
 
-CytronMD motor1(PWM_PWM, 4, 5);  // PWM 1 = Pin 3 1 M1A, DIR 1 = Pin 5 1 M1B.
-CytronMD motor2(PWM_PWM, 6, 7); // PWM 2 = Pin 4 1 M2A, DIR 2 = Pin 6 1 M2B.
+CytronMD motor1(PWM_PWM, M1A, M1B);  // PWM 1 = Pin 3 M1A, DIR 1 = Pin 5 M1B.
+CytronMD motor2(PWM_PWM, M2A, M2B); // PWM 2 = Pin 4 1 M2A, DIR 2 = Pin 6 1 M2B.
 
  
 void setup() {
@@ -19,6 +19,7 @@ void setup() {
   motor1.setSpeed(0);
   motor2.setSpeed(0);  
 }
+
 ISR(TIMER2_COMPA_vect)
 {
   sbus.process();
---
+++ Main/config.h
@@ -0,0 +1,10 @@
+#ifndef PIN_DEFINES_HPP
+#define PIN_DEFINES_HPP
+
+// Драйвер за мотори
+#define M1A 4
+#define M1B 5
+#define M2A 6
+#define M2B 7
+
+#endif 
