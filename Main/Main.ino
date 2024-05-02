90b6b6d6aefbd926019216f2d1608fa155340ae7
TOT102 <73033834+TOT102@users.noreply.github.com>
2024-05-02 08:53:04 +0200

    Update Main.ino
    Add pwm receiver library.

--- Main/Main.ino
+++ Main/Main.ino
@@ -2,10 +2,12 @@
 #include "config.h"
 #include <limits.h>
 #include "CytronMotorDriver.h"
- 
-int chanel[4];
-int n;
+#include <PWMreceiver.h>
 
+int pinNumbers[] = {A0, A1, A2, A3}; 
+size_t numPins = sizeof(pinNumbers) / sizeof(pinNumbers[0]);
+
+PWMReceiver receiver(pinNumbers, numPins);
 CytronMD motor1(PWM_PWM, M1A, M1B);  // PWM 1 = Pin 3 M1A, DIR 1 = Pin 5 M1B.
 CytronMD motor2(PWM_PWM, M2A, M2B); // PWM 2 = Pin 4 1 M2A, DIR 2 = Pin 6 1 M2B.
 
@@ -18,5 +20,22 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // polling:
+  receiver.readData();
+
+  if(receiver.getChannel() > 60){
+    //forward
+  }
+  else if(receiver.getChannel() < 40){
+    //backward
+  }
+  if(receiver.getChannel() > 60){
+    //clockwise
+  }
+  else if(receiver.getChannel() < 40){
+    //counter clockwise
+  }
+
+  
+
 }
